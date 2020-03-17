79f4247191df961863c3c3c519cc62623570cb86
Jessica Lam <jemlam@jessicas-mbp.lan>
2020-03-17 13:28:12 -0700

    created put and delete endpoints

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -37,6 +37,7 @@ router.get('/:id', (req, res) => {
         })
     })
 })
+//works on Postman
 
 router.post('/', (req, res) => {
     Posts.insert(req.body)
@@ -56,5 +57,48 @@ router.post('/', (req, res) => {
         })
     })
 })
+
+//works on Postman
+
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+    .then(posts => {
+        if (posts > 0) {
+            res.status(200).json({message: 'The post has been nuked'});
+        } else {
+            res.status(404).json({message: ' The post with the specified ID does not exist.'})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: 'The post could not be removed.'
+        })
+    })
+})
+
+//works on Postman
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    Posts.update(req.params.id, changes)
+    .then(post => {
+        if(post) {
+            res.status(200),json(post);
+        } else if (!post) {
+            res.status(404).json({message: 'The post with the specified ID does not exist.'})
+        } else {
+            res.status(400).json({errorMessage: 'Please provide title and contents for the post.'})
+        }
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: 'The post information could not be modified.'
+        });
+    });
+})
+
+//Receiveing error message, but is updating on Postman
 
 module.exports = router;
